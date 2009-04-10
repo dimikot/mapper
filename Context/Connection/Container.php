68730ccb9b84a0a5a641dca4a3d271d5f9358d3a
Dmitry Koterov <dmitry.koterov@gmail.com>
2009-04-10 13:53:18 +0400

    * Comments.

--- Context/Connection/Container.php
+++ Context/Connection/Container.php
@@ -25,39 +25,75 @@ abstract class Mapper_Context_Connection_Container implements DB_Micro_IConnecti
 		$this->_conn = $conn;
 	}
 
+    /**
+     * Begin transaction
+     *
+     */
     public function beginTransaction()
     {
         $this->_transactionStarted = true;
     	$this->_conn->beginTransaction();
     }
 
+    /**
+     * Commit
+     *
+     */
     public function commit()
     {
         $this->_transactionStarted = false;
         $this->_conn->commit();
     }
 
+    /**
+     * Rollback transaction
+     *
+     */
     public function rollBack()
     {
         $this->_transactionStarted = false;
         $this->_conn->rollBack();
     }
 
+    /**
+     * Executes query
+     *
+     * @param string $sql
+     * @param array $args
+     * @return array
+     */
     public function query($sql, $args = array())
     {
         return $this->_conn->query($sql, $args);
     }
 
+    /**
+     * Update query
+     *
+     * @param string $sql
+     * @param array $args
+     * @return array
+     */
     public function update($sql, $args = array())
     {
         return $this->_conn->update($sql, $args);
     }
 
+    /**
+     * Gets connection
+     *
+     * @return DB_Micro_IConnection
+     */
     protected function _getConnection()
     {
         return $this->_conn;
     }
 
+    /**
+     * Returns true, if transaction is started
+     *
+     * @return boolean
+     */
     protected function _transactionStarted()
     {
     	return $this->_transactionStarted;
